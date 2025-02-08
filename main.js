57f2e92c204d0780ced38f5da3a8d25c39708d20
Stefany <ste_giovanetti@yahoo.it>
2025-02-08 12:37:11 +0100

    Esercizio Condizioni 2 eseguito con lo switch

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 let temperatura = prompt ('Quanti gradi ci sono fuori?');
 
-if (temperatura < 20 && temperatura >= 0) {
-    console.log('Non ci sono più le mezze stagioni');
-} else if (temperatura >= 30) {
-    console.log('Lu mare, lu sole, lu ientu');
-} else if (temperatura < 30 && temperatura >= 20) {
-    console.log('Mi dia una peroni sudata');
-} else if (temperatura < 0 && temperatura >= -10) {
-    console.log('Non è tanto il freddo quanto l’umidità');
-} else if (temperatura < -10) {
-    console.log('Copriti… ancora ti raffreddi');
-} else {
-    console.log('Valore inserito non valido');
+switch (true) {
+    case temperatura < 20 && temperatura >= 0:
+        console.log('Non ci sono più le mezze stagioni');
+        break;
+
+    case temperatura >= 30:
+        console.log('Lu mare, lu sole, lu ientu');
+        break;
+
+    case temperatura < 30 && temperatura >= 20:
+        console.log('Mi dia una peroni sudata');
+        break;
+
+    case temperatura < 0 && temperatura >= -10:
+        console.log('Non è tanto il freddo quanto l’umidità');
+        break;
+
+    case temperatura < -10:
+        console.log('Copriti… ancora ti raffreddi');
+        break;
+
+    default:
+        console.log('Valore inserito non valido');
+        break;
 }
